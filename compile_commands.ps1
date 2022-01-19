0c930e830f83868bb3e437a1768336413604b5c6
SodaCris <18463922396@163.com>
2022-01-19 18:53:47 +0800

    add boost parser

---
+++ compile_commands.ps1
@@ -0,0 +1,2 @@
+# using MinGW 64 with vcpkg-installed boost
+g++ .\http_client.cpp -I D:\src\vcpkg\installed\x86-windows\include\ -l Ws2_32 -g
--- http_client.cpp
+++ http_client.cpp
@@ -1,14 +1,20 @@
 // #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "httplib.h"
 #include <cstdio>
+#include <boost/json/src.hpp>
+using namespace boost::json;
+
 int main()
 {
 
     // HTTP
     httplib::Client cli("http://localhost:8080");
-
-
     auto res = cli.Get("/size");
     printf("%d %s\n", res->status, res->body.c_str());
+    value jv = parse(res->body);
+    auto total = jv.at("total").as_double();
+    auto used = jv.at("used").as_double();
+    auto free = jv.at("free").as_double();
+    printf("total: %lf, used: %lf, free: %lf\n", total, used, free);
     return 0;
 }
